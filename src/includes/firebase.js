97dd5a48e2acda5941901beb2dd10ddfeeae3ea1
Bahaa <bhaaahmed188@gmail.com>
2023-04-27 20:34:17 +0200

    Upload files to storage and handle progress bar

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAFDs40vMR7zEjLOLwoLAjHAK1VjXXE0Vw",
@@ -14,7 +15,8 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, storage };
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import About from "../views/AboutView.vue";
 import Manage from "../views/ManageView.vue";
+import useUserStore from "../stores/user";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
